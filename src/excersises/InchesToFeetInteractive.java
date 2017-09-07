72ce2c04c72d59e407df4582d094ece4a63b55c6
Michael Bilenko <mbilenko03@users.noreply.github.com>
2017-09-07 08:58:16 -0500

    update

--- src/excersises/InchesToFeetInteractive.java
+++ src/excersises/InchesToFeetInteractive.java
@@ -1,12 +1,28 @@
 package excersises;
 
+import java.util.Scanner;
+
 public class InchesToFeetInteractive
 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		//conversion
+		final int inchesInFeet = 12;
 
+		//request user input
+		Scanner inputDevice = new Scanner(System.in);
+		System.out.print("How many inches: ");
+		int numberOfInches = inputDevice.nextInt();
+		inputDevice.close();
+			
+		//calculate conversions
+		int numberOfFeet = numberOfInches/inchesInFeet;
+		
+		int remainingInches = numberOfInches%inchesInFeet;
+		
+		//output
+		System.out.println("There are " + numberOfFeet + " feet and " + remainingInches + " inches in " + numberOfInches + " inches.");
 	}
 
 }
