c43cb1143ce9b9d4d0165b28e9c1f12aa1d9fb4f
Michael Bilenko <mbilenko03@users.noreply.github.com>
2017-09-07 08:45:45 -0500

    updated

--- src/excersises/Eggs.java
+++ src/excersises/Eggs.java
@@ -5,7 +5,6 @@ public class Eggs
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 
 	}
 
--- src/excersises/QuartsToGallonsInteractive.java
+++ src/excersises/QuartsToGallonsInteractive.java
@@ -1,11 +1,29 @@
 package excersises;
 
+import java.util.Scanner;
+
 public class QuartsToGallonsInteractive
 {
 
 	public static void main(String[] args)
 	{
+		//conversion
+		final int quartsInGallon = 4;
 
+		//request user input
+		Scanner inputDevice = new Scanner(System.in);
+		System.out.print("Please enter how many quarts you have: ");
+		int numberOfQuarts = inputDevice.nextInt();
+		inputDevice.close();
+			
+		//calculate conversions
+		int numberOfGallons = numberOfQuarts/quartsInGallon;
+		
+		int remainingQuarts = numberOfQuarts%quartsInGallon;
+		
+		//output
+		System.out.println("A job that needs " + numberOfQuarts + " quarts requires " + numberOfGallons 
+				+ " gallons plus " + remainingQuarts + " quarts." );
 	}
 
 }
