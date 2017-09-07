9117e50f1f2cb66b9b34aceba6e492c342fc3854
Michael Bilenko <mbilenko03@users.noreply.github.com>
2017-09-07 09:05:13 -0500

    update

--- src/excersises/Initials.java
+++ src/excersises/Initials.java
@@ -1,10 +1,24 @@
 package excersises;
 
+import java.util.Scanner;
+
 public class Initials
 {
 
 	public static void main(String[] args)
 	{
+		//request initials
+		Scanner inputDevice = new Scanner(System.in);
+		System.out.print("Please enter your first name: ");
+		char firstInitial = inputDevice.next().charAt(0);
+		System.out.print("Please enter your middle name: ");
+		char middleInitial = inputDevice.next().charAt(0);
+		System.out.print("Please enter your last name: ");
+		char lastInitial = inputDevice.next().charAt(0);
+		inputDevice.close();
+		
+		//display initials
+		System.out.println(firstInitial + "." + middleInitial + "." + lastInitial + ".");
 
 	}
 
