11ae68265dd1aeb76284cefe79b8971255603dc4
Michael Bilenko <mbilenko03@users.noreply.github.com>
2017-09-07 08:54:50 -0500

    update

--- src/excersises/NauticalMilesInteractive.java
+++ src/excersises/NauticalMilesInteractive.java
@@ -1,11 +1,28 @@
 package excersises;
 
+import java.util.Scanner;
+
 public class NauticalMilesInteractive
 {
 
 	public static void main(String[] args)
 	{
+		//conversion
+		final double kilometersInNauticalMiles = 1.852;
+		final double milesInNauticalMiles = 1.150779;
 
+		//request
+		Scanner inputDevice = new Scanner(System.in);
+		System.out.print("Please enter how many nautical miles: ");
+		double nauticalMiles = inputDevice.nextDouble();
+		inputDevice.close();
+		
+		//convert
+		double kilometers = nauticalMiles * kilometersInNauticalMiles;
+		double miles = nauticalMiles * milesInNauticalMiles;
+		
+		//output
+		System.out.println("In " + nauticalMiles + " there are " + kilometers + " kilometers and " + miles + " miles.");
 	}
 
 }
